121dada68c750cad28eefb79238821ba8bd138a1
ishiDACo <2542762+ishiDACo@users.noreply.github.com>
2020-02-03 18:56:54 +0900

    [#40] Fix load error by no cwdIDs and NVD-CWE-noinfo and NVD-CWE-Other handling

--- dist/js/vulsrepo.js
+++ dist/js/vulsrepo.js
@@ -531,27 +531,34 @@ const createPivotData = function(resultArray) {
                         let cweIds = y_val.cveContents.nvd.cweIDs;
                         let cweIdStr = "";
                         if (cweIds !== undefined) {
-                            // 安全なウェブサイトの作り方で言及されているものはマークを付ける
-                            let marks = ["113", "119", "22", "255", "264", "284", "287", "330", "352", "384", "425", "522", "548", "601", "614", "77", "78", "79", "89", "93"];
-                            for(var j = 0; j < cweIds.length; j++) {
-                                let match = false;
-                                for(var i = 0; i < marks.length; i++) {
-                                    if(cweIds[j].indexOf(marks[i]) !== -1) {
-                                        match = true;
-                                        break;
+                            // NVD-CWE-Other and NVD-CWE-noinfo
+                            if(cweIds[0].indexOf("NVD-CWE-") !== -1) {
+                                result["CweID"] = cweIds[0];
+                            } else {
+                                // 安全なウェブサイトの作り方で言及されているものはマークを付ける
+                                let marks = ["113", "119", "22", "255", "264", "284", "287", "330", "352", "384", "425", "522", "548", "601", "614", "77", "78", "79", "89", "93"];
+                                for(var j = 0; j < cweIds.length; j++) {
+                                    let match = false;
+                                    for(var i = 0; i < marks.length; i++) {
+                                        if(cweIds[j].indexOf(marks[i]) !== -1) {
+                                            match = true;
+                                            break;
+                                        }
+                                    }
+                                    if (match === true) {
+                                        cweIdStr = cweIdStr + cweIds[j] + "[!!]";
+                                    } else {
+                                        cweIdStr = cweIdStr + cweIds[j];
+                                    }
+                                    if (j < cweIds.length - 1) {
+                                        cweIdStr = cweIdStr + ",";
                                     }
                                 }
-                                if (match === true) {
-                                    cweIdStr = cweIdStr + cweIds[j] + "[!!]";
-                                } else {
-                                    cweIdStr = cweIdStr + cweIds[j];
-                                }
-                                if (j < cweIds.length - 1) {
-                                    cweIdStr = cweIdStr + ",";
-                                }
+                                result["CweID"] = "CHK-cweid-" + cweIdStr;
                             }
+                        } else {
+                            result["CweID"] = "None";
                         }
-                        result["CweID"] = "CHK-cweid-" + cweIdStr;
                     } else {
                         result["CweID"] = "None";
                     }
